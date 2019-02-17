7f3ed2e6acb30bb630b0f91a9e5eb3681d15491f
benbousquet <bbousque@asu.edu>
2019-02-16 20:07:25 -0700

    added ant design

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import Button from 'antd/lib/button';
+import './App.css';
 
 @inject('MainStore')
 @observer
@@ -9,6 +11,7 @@ class App extends Component {
     
     return (
       <div className="App">
+        <Button type="primary">Add Event</Button>
         <h2>Events: [{MainStore.eventCount}]</h2>
       </div>
     );
