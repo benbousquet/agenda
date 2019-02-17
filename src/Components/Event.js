a1246c1de8e82016e90367def3d7481da97029d2
JamesJass <jmjass3000@gmail.com>
2019-02-16 23:20:15 -0700

    Added events.js

---
+++ src/Components/Event.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Event(props){
+    return(
+        <React.Fragment>
+            <html shit>{props.date}</html>
+            <html shit>{props.eventName}</html>
+        </React.Fragment>
+    )
+}
+export default Event
