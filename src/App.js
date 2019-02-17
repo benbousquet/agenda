e70a865c4515361859b57ddaa19e15214f7e4571
benbousquet <bbousque@asu.edu>
2019-02-16 20:52:41 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react';
+import Home from './pages/Home';
+import Settings from './pages/Settings'
 import Button from 'antd/lib/button';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 
 @inject('MainStore')
 @observer
@@ -11,6 +19,19 @@ class App extends Component {
     
     return (
       <div className="App">
+        <Router>
+          <div>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/Settings">Settings</Link></li>
+            </ul>
+
+            <hr/>
+
+            <Route exact path="/" component={Home}/>
+            <Route path="/Settings" component={Settings}/>
+          </div>
+        </Router>
         <Button type="primary">Add Event</Button>
         <h2>Events: [{MainStore.eventCount}]</h2>
       </div>
